d2ff9df5603ce9375fed1894c45111867ba1bf1d
Ankur Patel <ankur.patel@ymail.com>
2015-02-21 22:21:08 -0500

    Init

---
+++ CarthageApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CarthageApp
+//
+//  Created by Ankur Patel on 2/21/15.
+//  Copyright (c) 2015 Encore Dev Labs LLC. All rights reserved.
+//
+
+import UIKit
+import Dollar
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println($.find([1, 2, 3], {$0 == 2}))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
